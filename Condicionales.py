3326c26e79079269ddc9c91f61ec917f40e67d3b
Alan <alanmendez200403@gmail.com>
2020-10-14 21:33:33 -0500

    Resolviendo errores y acomodando comandos

--- Condicionales.py
+++ Condicionales.py
@@ -4,11 +4,11 @@ if edad > 17:
 else:
     print('Eres menor de edad')
 
-    numero = int(input('Escribe un numero: '))
+numero = int(input('Escribe un numero: '))
 
-    if numero > 5:
-        print('Es mayor a 5')
-    elif numero ==5:
-        print('Es igual a 5')
-    else:
-        print('Es menor a 5')
+if numero > 5:
+     print('Es mayor a 5')
+elif numero ==5:
+    print('Es igual a 5')
+else:
+    print('Es menor a 5')
