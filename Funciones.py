d4b3527a15503b2d51c2ac9a5dd489df31b96e5d
Alan <alanmendez200403@gmail.com>
2020-10-15 09:23:51 -0500

    Funciones.py

---
+++ Funciones.py
@@ -0,0 +1,9 @@
+# Con esto usamos creamos una funcion
+def funcion1():
+    print("Mensaje especial: ")
+    print("Usando funciones :)")
+
+#Ahora la repetimos 3 veces!!!
+funcion1()
+funcion1()
+funcion1()
